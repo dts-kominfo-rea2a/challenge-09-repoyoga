6581ae20e4a20731c30057f9f4e9cc78c123bc36
Restiawan Yoga Pratama <restiawan008@gmail.com>
2022-06-20 20:47:27 +0700

    tantangan intro React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css'
+import '../data/contacts.json'
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
+        <div className='container'>
+            <div className='card'>
+                <img className='foto' src='{data.photo}' alt='foto'></img>
+                <div className='content'>
+                    <h2>{data.name}</h2>
+                    <p>{data.phone}</p>
+                    <p>{data.email}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <></>
+        <div className='header'>
+            <h1 className='title'>Call a Friend</h1>
+            <p className='tagline'>your friendly contact app</p>
+            <p className='titik'>...........................................................</p>
+        </div>
     )
 }
 
